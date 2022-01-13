4b27758d9b3af444047ebc4d11061f7d64558e69
Tayhana Marçal Rangel da Fonseca <85115577+Tayrangel@users.noreply.github.com>
2022-01-13 20:42:23 -0300

    Delete reactjs directory

--- reactjs/src/components/Filho.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default props =>
-    <>
-    <p>{props.children} {props.sobrenome}</p>
-    </>
--- reactjs/src/components/Pai.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Filho from './Filho'
-
-export default props =>
-    <div>
-    <Filho {...props}>Filho 1</Filho>
-    <Filho sobrenome={props.sobrenome}>Filho 2</Filho>
-    <Filho sobrenome="Maroni">Filho 3</Filho>
-    </div>
